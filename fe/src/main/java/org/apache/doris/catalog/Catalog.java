8c144318e71731037d454082bbec0c90e4fb2c6c
Mingyu Chen <morningman.cmy@gmail.com>
2020-06-08 23:00:36 +0800

    [Bug] Fix bug that checkpoint thread failed to start (#3795)
    1. Set thread id before starting the checkpoint thread
    2. Init the CHECKPOINT catalog instance before visiting it.

--- fe/src/main/java/org/apache/doris/catalog/Catalog.java
+++ fe/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -540,16 +540,13 @@ public class Catalog {
         }
     }
 
-    // use this to get CheckPoint Catalog instance
-    public static Catalog getCheckpoint() {
-        if (CHECKPOINT == null) {
-            CHECKPOINT = new Catalog();
-        }
-        return CHECKPOINT;
-    }
-
     public static Catalog getCurrentCatalog() {
         if (isCheckpointThread()) {
+            // only checkpoint thread it self will goes here.
+            // so no need to care about the thread safe.
+            if (CHECKPOINT == null) {
+                CHECKPOINT = new Catalog();
+            }
             return CHECKPOINT;
         } else {
             return SingletonHolder.INSTANCE;
@@ -1203,8 +1200,11 @@ public class Catalog {
         // start checkpoint thread
         checkpointer = new Checkpoint(editLog);
         checkpointer.setMetaContext(metaContext);
-        checkpointer.start();
+        // set "checkpointThreadId" before the checkpoint thread start, because the thread
+        // need to check the "checkpointThreadId" when running.
         checkpointThreadId = checkpointer.getId();
+
+        checkpointer.start();
         LOG.info("checkpointer thread started. thread id is {}", checkpointThreadId);
 
         // heartbeat mgr
