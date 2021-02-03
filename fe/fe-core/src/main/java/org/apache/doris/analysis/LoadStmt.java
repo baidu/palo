1daf2080952a7f6b718612efe0bc6dd714ce1655
miaoling01 <miaoling01@baidu.com>
2021-02-03 21:00:04 +0800

    [Internal][Bug-fix][doris-1075] Readd cluster property in load stmt
    The cluster property has been removed from load stmt which is incorrect.
    It caused user could not create hadoop load.
    
    The PR readd cluster property in load stmt to fix error.
    
    Change-Id: I5c5c75b48c194a0313f12551e52a25e8d967bc18

--- fe/fe-core/src/main/java/org/apache/doris/analysis/LoadStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/LoadStmt.java
@@ -161,6 +161,12 @@ public class LoadStmt extends DdlStmt {
                     return Integer.valueOf(s);
                 }
             })
+            .put(CLUSTER_PROPERTY, new Function<String, Integer>() {
+                @Override
+                public @Nullable Integer apply(@Nullable String s) {
+                    return Integer.valueOf(s);
+                }
+            })
             .build();
 
     public LoadStmt(LabelName label, List<DataDescription> dataDescriptions,
