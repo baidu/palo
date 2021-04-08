1acb331702cd30a7091c07d343a1c5acb00fed93
luozenglin <37725793+luozenglin@users.noreply.github.com>
2021-04-08 09:09:56 +0800

    [Enhance] Filter all non-journal db when getting journal db names (#5605)
    Change-Id: I03264c9ef86b8a556ed21489bb733a21d10702cd

--- fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBEnvironment.java
+++ fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBEnvironment.java
@@ -43,6 +43,7 @@ import com.sleepycat.je.rep.StateChangeListener;
 import com.sleepycat.je.rep.util.DbResetRepGroup;
 import com.sleepycat.je.rep.util.ReplicationGroupAdmin;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -347,11 +348,11 @@ public class BDBEnvironment {
         
         if (names != null) {
             for (String name : names) {
-                try {
-                    long db = Long.parseLong(name);
-                    ret.add(db);
-                } catch (NumberFormatException e) {
+                if (StringUtils.isNumeric(name)) {
+                    ret.add(Long.parseLong(name));
+                } else {
                     // "epochDB" and "metricDB" will throw this exception. No need to deal with it.
+                    LOG.debug("get database names, skipped {}", name);
                 }
             }
         }
