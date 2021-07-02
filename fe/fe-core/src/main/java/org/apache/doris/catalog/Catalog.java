3e6b2a02d2bcad4cb4c6be5a561a0f6e7d29d2e4
Mingyu Chen <morningman.cmy@gmail.com>
2021-07-02 19:24:13 +0800

    [Bug] Fix bug of NPE caused by the absence of table in replay process. (#6136)
    In the previous version, we changed the db level lock to the table level
    to reduce lock contention. But this change will cause some metadata playback problems.
    
    Because most operations on a table will only acquire table locks. These operations
    may occur at the same time as the operation of dropping the table.
    This may cause the metadata operation sequence to be inconsistent with the log replay sequence,
    which may cause some problems.

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Catalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -3430,6 +3430,10 @@ public class Catalog {
     public void replayDropPartition(DropPartitionInfo info) {
         Database db = this.getDb(info.getDbId());
         OlapTable olapTable = (OlapTable) db.getTable(info.getTableId());
+        if (olapTable == null) {
+            LOG.warn("table {} does not exist when replaying drop rollup. db: {}", info.getTableId(), db.getId());
+            return;
+        }
         olapTable.writeLock();
         try {
             if (info.isTempPartition()) {
@@ -3450,6 +3454,10 @@ public class Catalog {
         long dbId = info.getDbId();
         Database db = getDb(dbId);
         Table table = db.getTable(info.getTableId());
+        if (table == null) {
+            LOG.warn("table {} does not exist when replaying drop rollup. db: {}", info.getTableId(), db.getId());
+            return;
+        }
         table.writeLock();
         try {
             Catalog.getCurrentRecycleBin().replayRecoverPartition((OlapTable) table, info.getPartitionId());
@@ -5246,7 +5254,7 @@ public class Catalog {
         }
     }
 
-    // the invoker should keep db write lock
+    // the invoker should keep table's write lock
     public void modifyTableColocate(Database db, OlapTable table, String colocateGroup, boolean isReplay,
             GroupId assignedGroupId)
             throws DdlException {
@@ -5331,6 +5339,11 @@ public class Catalog {
 
         Database db = getDb(info.getGroupId().dbId);
         OlapTable table = (OlapTable) db.getTable(tableId);
+        if (table == null) {
+            LOG.warn("table {} does not exist when replaying modify table colocate. db: {}",
+                    tableId, info.getGroupId().dbId);
+            return;
+        }
         table.writeLock();
         try {
             modifyTableColocate(db, table, properties.get(PropertyAnalyzer.PROPERTIES_COLOCATE_WITH), true,
@@ -5391,6 +5404,10 @@ public class Catalog {
 
         Database db = getDb(dbId);
         OlapTable table = (OlapTable) db.getTable(tableId);
+        if (table == null) {
+            LOG.warn("table {} does not exist when replaying rename rollup. db: {}", tableId, dbId);
+            return;
+        }
         table.writeLock();
         try {
             String rollupName = table.getIndexNameById(indexId);
@@ -5444,7 +5461,7 @@ public class Catalog {
         }
     }
 
-    public void replayRenamePartition(TableInfo tableInfo) throws DdlException {
+    public void replayRenamePartition(TableInfo tableInfo) {
         long dbId = tableInfo.getDbId();
         long tableId = tableInfo.getTableId();
         long partitionId = tableInfo.getPartitionId();
@@ -5452,6 +5469,10 @@ public class Catalog {
 
         Database db = getDb(dbId);
         OlapTable table = (OlapTable) db.getTable(tableId);
+        if (table == null) {
+            LOG.warn("table {} does not exist when replaying rename partition. db: {}", tableId, dbId);
+            return;
+        }
         table.writeLock();
         try {
             Partition partition = table.getPartition(partitionId);
@@ -5497,7 +5518,7 @@ public class Catalog {
      * @param properties
      * @throws DdlException
      */
-    // The caller need to hold the db write lock
+    // The caller need to hold the table's write lock
     public void modifyTableReplicationNum(Database db, OlapTable table, Map<String, String> properties) throws DdlException {
         Preconditions.checkArgument(table.isWriteLockHeldByCurrentThread());
         String defaultReplicationNumName = "default."+ PropertyAnalyzer.PROPERTIES_REPLICATION_NUM;
@@ -5532,7 +5553,7 @@ public class Catalog {
      * @param table
      * @param properties
      */
-    // The caller need to hold the db write lock
+    // The caller need to hold the table's write lock
     public void modifyTableDefaultReplicationNum(Database db, OlapTable table, Map<String, String> properties) {
         Preconditions.checkArgument(table.isWriteLockHeldByCurrentThread());
         TableProperty tableProperty = table.getTableProperty();
@@ -5576,6 +5597,10 @@ public class Catalog {
 
         Database db = getDb(dbId);
         OlapTable olapTable = (OlapTable) db.getTable(tableId);
+        if (olapTable == null) {
+            LOG.warn("table {} does not exist when replaying modify table property log. db: {}", tableId, dbId);
+            return;
+        }
         olapTable.writeLock();
         try {
             TableProperty tableProperty = olapTable.getTableProperty();
@@ -6654,6 +6679,11 @@ public class Catalog {
     public void replayTruncateTable(TruncateTableInfo info) {
         Database db = getDb(info.getDbId());
         OlapTable olapTable = (OlapTable) db.getTable(info.getTblId());
+        if (olapTable == null) {
+            LOG.warn("table {} does not exist when replaying truncate table log. db id: {}",
+                    info.getTblId(), info.getDbId());
+            return;
+        }
         olapTable.writeLock();
         try {
             truncateTableInternal(olapTable, info.getPartitions(), info.isEntireTable());
@@ -6804,6 +6834,11 @@ public class Catalog {
     public void replayConvertDistributionType(TableInfo tableInfo) {
         Database db = getDb(tableInfo.getDbId());
         OlapTable tbl = (OlapTable) db.getTable(tableInfo.getTableId());
+        if (tbl == null) {
+            LOG.warn("table {} does not exist when replaying convert distribution type. db: {}",
+                    tableInfo.getTableId(), tableInfo.getDbId());
+            return;
+        }
         tbl.writeLock();
         try {
             tbl.convertRandomDistributionToHashDistribution();
