d53cbef36e102690e338fbf290c643f089c2d410
chenmingyu <chenmingyu@baidu.com>
2021-11-12 15:19:23 +0800

    0.15.1-rc09
    Change-Id: Id05de8ff8e301966a8435ec7ef7e9a0f15e9765b

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="trunk"
+build_version="0.15.1-rc09"
 
 unset LANG
 unset LC_CTYPE
